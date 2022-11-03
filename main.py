8f4d365121444b988a2e22dc99bbda996e391771
Rachel Quijano <71981920+rachelquijano@users.noreply.github.com>
2022-11-03 16:33:32 -0400

    Added second feature + refactoring

--- main.py
+++ main.py
@@ -28,7 +28,18 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 song_library_features = pd.read_csv('csv/track_features.csv', sep=',', header=None)
 song_library_size = len(song_library_features)
+
+
 feature_variation_percentage = 0.25
+library_matches_percentage = 0.50
+
+
+def compareFeature(libraryfeature, chosenSongFeature):
+    upper_feature_range = libraryfeature + (
+                libraryfeature * feature_variation_percentage)
+    lower_feature_range = libraryfeature - (
+                libraryfeature * feature_variation_percentage)
+    return 1 if(chosenSongFeature >= lower_feature_range and chosenSongFeature <= upper_feature_range) else 0
 
 
 # User Interaction
@@ -46,52 +57,61 @@ song_results =  sp.search(q ='track:' + song_input, type='track')
 items_array = song_results["tracks"]["items"]
 
 #specify the first result as our chosen song
-song_uri = items_array[0]['uri']
+if len(items_array) > 0:
+    song_uri = items_array[0]['uri']
 
+    #if the song uri has been acquired and the input is correct
+    if song_input:
+        if song_uri:
+            # get the track object from the given uri
+            song = sp.track(song_uri)
+            artist_info = song['artists']
+            st.write('You have chosen the song:', song['name'], "by", artist_info[0]['name'])
+            st.write('Here are some of your songs categories')
+            song_features = sp.audio_features(song_uri)[0]
+            st.write(song_features)
 
-#if the song uri has been acquired
-if song_input:
-    if song_uri:
-        # get the track object from the given uri
-        song = sp.track(song_uri)
-        artist_info = song['artists']
-        st.write('You have chosen the song:', song['name'], "by", artist_info[0]['name'])
-        st.write('Here are some of your songs categories')
-        song_features = sp.audio_features(song_uri)[0]
-        st.write(song_features)
+            danceability = st.checkbox("View danceability")
 
+            #If the user has chosen to view the danceability feature
+            if(danceability):
+                number_of_danceability_matches = 0
+                #get the chosen song's danceability
+                song_danceability = song_features['danceability']
+                #how many times does the chosen song match the library songs
 
-        st.write("We will now analyze your song's details with our library, please wait!")
-
-        danceability = st.checkbox("View danceability")
-
-        #If the user has chosen to view the danceability feature
-        if(danceability):
-            #get the chosen song's danceability
-            song_danceability = song_features['danceability']
-            #how many times does the chosen song match the library songs
-            number_of_matches = 0
-            #iterating through all the danceabilities, column #0
-            for index, song in song_library_features.iterrows():
-                #getting the danceability feature
-                currentLibrarySongDanceability = song[0]
-                #creating ranges for how much the chosen song can vary from the library song
-                upper_danceability_range = currentLibrarySongDanceability + (currentLibrarySongDanceability * feature_variation_percentage)
-                lower_danceability_range = currentLibrarySongDanceability - (currentLibrarySongDanceability * feature_variation_percentage)
-
-                #st.write("The chosen song has a danceability of",song_danceability, "The current library song is:", sp.track(song[13])['name'], "which has a danceability of", currentLibrarySongDanceability)
-                if song_danceability >= lower_danceability_range and song_danceability <= upper_danceability_range:
-                    st.write("The chosen song matches!!")
-                    number_of_matches += 1
+                #iterating through all the danceabilities, column #0
+                for index, song in song_library_features.iterrows():
+                    #getting the danceability feature
+                    currentLibrarySongFeature = song[0]
+                    number_of_danceability_matches += compareFeature(song_danceability, currentLibrarySongFeature)
+                st.write("The song matches", number_of_danceability_matches, "of the songs in our library of", song_library_size, "songs")
+                if number_of_danceability_matches / song_library_size >= library_matches_percentage:
+                    st.success("This song matches in danceability to our library!")
                 else:
-                    st.write("The chosen song does NOT match")
-            st.write("The song matches", number_of_matches, "of the songs in our library of", song_library_size, "songs")
-            if number_of_matches / song_library_size >= 0.50:
-                st.write("This song matches in danceability to our library!")
-            else:
-                st.write("This song does NOT match in danceability to our library!")
-    else:
-        st.write("Sorry, we could not find that song on our database :( Please try a different search!")
+                    st.warning("This song does NOT match in danceability to our library!")
+
+            energy = st.checkbox("View energy")
+            if(energy):
+                number_of_energy_matches = 0
+                #get the chosen song's danceability
+                song_energy = song_features['energy']
+                #how many times does the chosen song match the library songs
+
+                #iterating through all the danceabilities, column #0
+                for index, song in song_library_features.iterrows():
+                    #getting the danceability feature
+                    currentLibrarySongFeature = song[0]
+                    number_of_energy_matches += compareFeature(song_energy, currentLibrarySongFeature)
+                st.write("The song matches", number_of_energy_matches, "of the songs in our library of", song_library_size, "songs")
+                if number_of_energy_matches / song_library_size >= library_matches_percentage:
+                    st.success("This song matches in energy to our library!")
+                else:
+                    st.warning("This song does NOT match in energy to our library!")
+
+
+else:
+    st.error("Sorry, we could not find that song on our database :( Please try a different search!")
 
 
 
